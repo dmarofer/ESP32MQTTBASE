13f614f0353911f6976040a85282b23ba1714c3d
Diego Maroto <dmarofer@diegomaroto.net>
2019-04-02 19:03:12 +0200

    Actualizado

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,10 @@
 #pragma region COMENTARIOS
 
 /*
-RIEGAMATICO_MQTT
-Controlador de Riego con ESP32 y comunicaciones Wifi MQTT
-Pensado para ser gestionado y controlado desde HomeAssistant
+ESP32MQTTBASE
+
+Proyecto para usar de base en tus proyectos con ESP32 
+Incluye gestion de la configuracion Wifi y MQTT por puerto serie y HTTP (WifiManager) Incluye gestion de tareas con FreeRTOS
 
 Autor: Diego Maroto - BilbaoMakers 2019 - info@bilbaomakers.org
 
